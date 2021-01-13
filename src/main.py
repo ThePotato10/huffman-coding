d56ed2d9d3084ef13bf117d3c49a711e5c060d75
ThePotato10 <63891976+ThePotato10@users.noreply.github.com>
2021-01-13 12:18:28 -0500

    Update and rename console.py to main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ from tree import generate_tree
 
 phrase = "Hello, World!"
 
+# Generates the huffman coding tree using a given string input
 letter_tree = generate_tree(phrase)
+
+# prints the string after going through encoding
 print(encode(phrase, letter_tree))
+
+# prints the decoded string based on a tree and an encoded string
 print(decode(encode(phrase, letter_tree), letter_tree))
